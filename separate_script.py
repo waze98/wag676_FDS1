f361b3da6ce9a5248373a1826d5fc0b7ee5980d6
waze98 <wgeronimo@utexas.edu>
2023-12-11 18:05:59 -0600

    Second part scripts

---
+++ separate_script.py
@@ -0,0 +1,13 @@
+from unit_conversions import *
+test_num = 20
+print("Kilometers and Miles")
+print(f"{test_num} mi = {mi_to_km(test_num)} km")
+print(f"{test_num} km = {km_to_mi(test_num)} mi")
+
+print("\n\nFahrenheit and Celcius")
+print(f"{test_num} deg F = {f_to_c(test_num)} deg C")
+print(f"{test_num} deg C = {c_to_f(test_num)} deg F")
+
+print("\n\nKilograms and Pounds")
+print(f"{test_num} kg = {kg_to_lb(test_num)} lb(s)")
+print(f"{test_num} lb(s) = {lb_to_kg(test_num)} kg")
---
+++ unit_conversions.py
@@ -0,0 +1,17 @@
+def mi_to_km(mi):
+    return mi * 1.60934
+
+def km_to_mi(km):
+    return km * 0.621371
+
+def f_to_c(f):
+    return (f - 32) * 5/9
+
+def c_to_f(c):
+    return c * 9/5 + 32
+
+def kg_to_lb(kg):
+    return kg * 2.20462
+
+def lb_to_kg(lb):
+    return lb * 0.453592
